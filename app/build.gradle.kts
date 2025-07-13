0cc1b4ba92d0981ce34bfd722e9ad1ecb32eae65
johanna <167251945+johannabruns@users.noreply.github.com>
2025-07-13 14:56:26 +0200

    build gradle update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,6 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 
-        id("com.android.application")
-        id("org.jetbrains.kotlin.android")
         id("org.jetbrains.kotlin.kapt")
 }
 
